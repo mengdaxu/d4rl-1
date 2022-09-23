00cf18ec42264de53b649bfe7a2bc7601c93fbe4
Mengda <mdxu1031@gmail.com>
2022-09-23 01:29:54 -0400

    ignore import error

--- d4rl/__init__.py
+++ d4rl/__init__.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 
 import d4rl.locomotion
-import d4rl.hand_manipulation_suite
 import d4rl.pointmaze
 import d4rl.gym_minigrid
 import d4rl.gym_mujoco
@@ -12,6 +11,15 @@ from d4rl.offline_env import set_dataset_path, get_keys
 SUPPRESS_MESSAGES = bool(os.environ.get('D4RL_SUPPRESS_IMPORT_ERROR', 0))
 
 _ERROR_MESSAGE = 'Warning: %s failed to import. Set the environment variable D4RL_SUPPRESS_IMPORT_ERROR=1 to suppress this message.'
+
+
+try:
+    import d4rl.hand_manipulation_suite
+except ImportError as e:
+    if not SUPPRESS_MESSAGES:
+        print(_ERROR_MESSAGE % 'hand_manipulation_suite', file=sys.stderr)
+        print(e, file=sys.stderr)
+
 
 try:
     import d4rl.flow
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
     name='d4rl',
     version='1.1',
-    install_requires=['gym', 
-                      'numpy', 
-                      'mujoco_py', 
-                      'h5py', 
-                      'termcolor', # adept_envs dependency
-                      'click',  # adept_envs dependency
-                      'dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+    install_requires=[
+        'gym',
+        'numpy',
+        'mujoco_py',
+        'h5py',
+        'termcolor',  # adept_envs dependency
+        'click',  # adept_envs dependency
+    ],
     packages=find_packages(),
 )
